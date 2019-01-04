aaa506c1f267e8fdfa9f1d1f20f4587681beaf81
Robert Snyder <r.snyder49@gmail.com>
2019-01-04 22:21:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 // Code your solution in this file
 function findMatching(array, str) {
-  let newArray = [];
+  let matchArr = [];
   
   for (let i = 0; i < array.length; i++) {
      if (array[i].toLowerCase() === str.toLowerCase()) {
        newArray.push(array[i]);
     }
   }
-  return newArray;
+  return matchArr;
 }
 
 function fuzzyMatch(array, str) {
-  let newArray = [];
+  let fuzzArr = [];
   
   for (const name of array) {
     if (name.toLowerCase().slice(0, str.length) === str.toLowerCase()) {
       array.push(name);
     }
   }
-  return newArray;
+  return fuzzArr;
 }
 
 function matchName(array, str) {
-  let newArray = [];
+  let nameArr = [];
    
   for (const driver of array) {
     if (driver.name.toLowerCase() === str.toLowerCase() ) {
        array.push(driver);
     }
   }
-  return newArray;
+  return nameArr;
 }
